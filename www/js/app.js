fa9fe72cc1e6c261df701cf70d517d5e6bc4522b
mbedoya <mauricio.bedoya@gmail.com>
2015-10-26 08:09:53 -0500

    playlists removed

--- www/js/app.js
+++ www/js/app.js
@@ -52,15 +52,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
                     }
                 }
             })
-            .state('app.playlists', {
-                url: '/playlists',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'templates/playlists.html',
-                        controller: 'PlaylistsCtrl'
-                    }
-                }
-            })
 
             .state('app.menu.contacts', {
                 url: '/contacts',
@@ -82,15 +73,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
                 }
             })
 
-            .state('app.single', {
-                url: '/playlists/:playlistId',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'templates/playlist.html',
-                        controller: 'PlaylistCtrl'
-                    }
-                }
-            });
+            ;
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/app/welcome');
     });
