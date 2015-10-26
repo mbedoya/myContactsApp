546ec0646f4bc1b585b1ad6ebbb993f133ec439a
mbedoya <mauricio.bedoya@gmail.com>
2015-10-26 08:07:53 -0500

    browse deleted

--- www/js/app.js
+++ www/js/app.js
@@ -6,33 +6,33 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/app.html',
-    controller: 'AppCtrl'
-  })
-  
-  .state('app.menu', {
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'templates/app.html',
+                controller: 'AppCtrl'
+            })
+
+            .state('app.menu', {
                 url: "/menu",
                 abstract: true,
                 views: {
@@ -42,64 +42,55 @@ angular.module('starter', ['ionic', 'starter.controllers'])
                     }
                 }
             })
-  
-   .state('app.welcome', {
-    url: '/welcome',
-    views: {
-       'app-view': {
-           templateUrl: 'templates/welcome.html',
-           controller: 'WelcomeCtrl'    
-       }
-    }
-  })
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-    
-    .state('app.menu.contacts', {
-      url: '/contacts',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/contacts.html',
-          controller: 'ContactsCtrl'
-        }
-      }
-    })
+            .state('app.welcome', {
+                url: '/welcome',
+                views: {
+                    'app-view': {
+                        templateUrl: 'templates/welcome.html',
+                        controller: 'WelcomeCtrl'
+                    }
+                }
+            })
+            .state('app.playlists', {
+                url: '/playlists',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/playlists.html',
+                        controller: 'PlaylistsCtrl'
+                    }
+                }
+            })
 
-      .state('app.menu.contact', {
-          url: '/contact',
-          views: {
-              'menuContent': {
-                  templateUrl: 'templates/contact.html',
-                  controller: 'ContactCtrl'
-              }
-          }
-      })
+            .state('app.menu.contacts', {
+                url: '/contacts',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/contacts.html',
+                        controller: 'ContactsCtrl'
+                    }
+                }
+            })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/welcome');
-});
+            .state('app.menu.contact', {
+                url: '/contact',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/contact.html',
+                        controller: 'ContactCtrl'
+                    }
+                }
+            })
+
+            .state('app.single', {
+                url: '/playlists/:playlistId',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/playlist.html',
+                        controller: 'PlaylistCtrl'
+                    }
+                }
+            });
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/welcome');
+    });
