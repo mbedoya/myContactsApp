21496609b24077be05e3825b67ada177e9d38cb8
mbedoya <mauricio.bedoya@gmail.com>
2015-11-03 17:51:09 -0500

    setcontacts test

--- www/js/services.js
+++ www/js/services.js
@@ -37,12 +37,16 @@ angular.module('laboru.services', [])
                 //Map Contacts
                 contactsArray = new Array();
 
+                alert("About to set Contacts in array");
+
                 for(i=0; i<contacts.length; i++){
                     //Add Contacts if they have a name and mobile number
                     if(contacts[i].displayName && contacts[i].name.givenName && contacts[i].phoneNumbers.length > 0){
                         contactsArray.push({Name : contacts[i].name.givenName, LastName : contacts[i].name.familiyName, Mobile: contacts[i].phoneNumbers[0].value });
                     }
                 }
+
+                alert("Contacts in array");
 
                 $.ajax({
                     url: serviceURL,
