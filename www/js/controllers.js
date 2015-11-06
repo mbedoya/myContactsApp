6389aeabc67029a8d6ac6976e74838b9354341e5
mbedoya <mauricio.bedoya@gmail.com>
2015-11-06 08:07:47 -0500

    find experts prototype auto search

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -226,28 +226,159 @@ angular.module('laboru.controllers', [])
 
     })
 
-    .controller('ExpertsCtrl', function($scope, $rootScope, $location, Utility) {
+    .controller('ExpertsCtrl', function($scope, $rootScope, $ionicLoading, $location, Utility) {
 
-        $scope.skills = new Array();
-        $scope.selectedSkills = new Array();
+        $scope.initialize = function(){
+
+            $scope.filteredSkills = new Array();
+            $scope.skills = new Array();
+            $scope.selectedSkills = new Array();
+
+            $scope.skills.push('Almacenista');
+            $scope.skills.push('Avicultor');
+            $scope.skills.push('Bailarín');
+            $scope.skills.push('Belleza/Depilación');
+            $scope.skills.push('Belleza/Manicure Pedicure');
+            $scope.skills.push('Belleza/Maquillaje');
+            $scope.skills.push('Belleza/Masajes');
+            $scope.skills.push('Belleza/Peluquería');
+            $scope.skills.push('Bombero');
+            $scope.skills.push('Cantante');
+            $scope.skills.push('Carnicero');
+            $scope.skills.push('Cartero');
+            $scope.skills.push('Conductor');
+            $scope.skills.push('Deporte/Instructor(a) Ciclismo');
+            $scope.skills.push('Deporte/Instructor(a) Equitación');
+            $scope.skills.push('Deporte/Instructor(a) Natación');
+            $scope.skills.push('Deporte/Instructor(a) Squash');
+            $scope.skills.push('Deporte/Instructor(a) Tenis de Campo');
+            $scope.skills.push('Deporte/Instructor(a) Tenis de Mesa');
+            $scope.skills.push('Educación/Profesor(a) Alemán');
+            $scope.skills.push('Educación/Profesor(a) Español');
+            $scope.skills.push('Educación/Profesor(a) Física');
+            $scope.skills.push('Educación/Profesor(a) Inglés');
+            $scope.skills.push('Educación/Profesor(a) Italiano');
+            $scope.skills.push('Educación/Profesor(a) Latín');
+            $scope.skills.push('Educación/Profesor(a) Mandarín');
+            $scope.skills.push('Educación/Profesor(a) Matemáticas');
+            $scope.skills.push('Educación/Profesor(a) Portugués');
+            $scope.skills.push('Educación/Profesor(a) Química');
+            $scope.skills.push('Escultor');
+            $scope.skills.push('Fotógrafo');
+            $scope.skills.push('Hogar/Albañilería');
+            $scope.skills.push('Hogar/Carpintería');
+            $scope.skills.push('Hogar/Cerrajería');
+            $scope.skills.push('Hogar/Cortinería');
+            $scope.skills.push('Hogar/Cuidado Niños');
+            $scope.skills.push('Hogar/Decoración');
+            $scope.skills.push('Hogar/Electricidad');
+            $scope.skills.push('Hogar/Jardinería');
+            $scope.skills.push('Hogar/Pintura');
+            $scope.skills.push('Hogar/Plomería');
+            $scope.skills.push('Interventor');
+            $scope.skills.push('Locutor');
+            $scope.skills.push('Mascotas/Adiestramiento');
+            $scope.skills.push('Mascotas/Baño Peluquería');
+            $scope.skills.push('Mascotas/Paseador(a) Canino');
+            $scope.skills.push('Mascotas/Veterinaria');
+            $scope.skills.push('Matemático');
+            $scope.skills.push('Medicina/Anestesiología');
+            $scope.skills.push('Medicina/Bacteriología');
+            $scope.skills.push('Medicina/Cardiología');
+            $scope.skills.push('Medicina/Cirugía');
+            $scope.skills.push('Medicina/Dermatología');
+            $scope.skills.push('Medicina/Enfermería');
+            $scope.skills.push('Medicina/Fisioterapia');
+            $scope.skills.push('Medicina/Geriatría');
+            $scope.skills.push('Medicina/Ginecología');
+            $scope.skills.push('Medicina/Medico General');
+            $scope.skills.push('Medicina/Nutrición');
+            $scope.skills.push('Medicina/Odontología');
+            $scope.skills.push('Medicina/Oftalmología');
+            $scope.skills.push('Medicina/Ortodoncia');
+            $scope.skills.push('Medicina/Ortopedia');
+            $scope.skills.push('Medicina/Pediatría');
+            $scope.skills.push('Medicina/Radiología');
+            $scope.skills.push('Medicina/Urología');
+            $scope.skills.push('Mesero');
+            $scope.skills.push('Paisajista');
+            $scope.skills.push('Panadero');
+            $scope.skills.push('Perito');
+            $scope.skills.push('Pianista');
+            $scope.skills.push('Profesión/Actor');
+            $scope.skills.push('Profesión/Actriz');
+            $scope.skills.push('Profesión/Agronomía');
+            $scope.skills.push('Profesión/Antropología');
+            $scope.skills.push('Profesión/Arquitectura');
+            $scope.skills.push('Profesión/Contaduría');
+            $scope.skills.push('Profesión/Derecho');
+            $scope.skills.push('Profesión/Diseño Gráfico');
+            $scope.skills.push('Profesión/Gastronomía');
+            $scope.skills.push('Profesión/Ingeniería Cívil');
+            $scope.skills.push('Profesión/Periodismo');
+            $scope.skills.push('Profesión/Publicidad');
+            $scope.skills.push('Profesión/Topografía');
+            $scope.skills.push('Recreacionista');
+            $scope.skills.push('Tecnología/Auditoria Sistemas Informáticos');
+            $scope.skills.push('Tecnología/Desarollo Apps');
+            $scope.skills.push('Tecnología/Desarrollo Web');
+            $scope.skills.push('Tecnología/Redes');
+            $scope.skills.push('Tecnología/Seguridad Informática');
+            $scope.skills.push('Tecnología/Telecomunicaciones');
+            $scope.skills.push('Traductor');
+            $scope.skills.push('Vigilante');
+            $scope.skills.push('Violinista');
+
+        }
+
+        $scope.initialize();
+
+        $scope.clear = function(){
+            $scope.selectedSkills.length = 0;
+            $scope.filteredSkills.length = 0;
+            $scope.data.search = "";
+            $scope.showExperts = false;
+        }
+
+        $scope.removeSkill = function(index){
+            $scope.selectedSkills.splice(index, 1);
+            if($scope.selectedSkills.length == 0){
+                $scope.showExperts = false;
+            }else{
+                $scope.searchExperts();
+            }
+        }
         
         $scope.selectSkill = function(skill){
             $scope.selectedSkills.push(skill);
             $scope.data.search = "";
-            $scope.skills.length = 0;
+            $scope.filteredSkills.length = 0;
+
+            $scope.searchExperts();
+        }
+
+        $scope.filterSkills = function(value, index, ar){
+            return value.toLowerCase().indexOf($scope.data.search.toLowerCase()) >= 0;
         }
 
         $scope.searchSkill = function(){
-            $scope.skills.length = 0;
-
-            if($scope.data.search && $scope.data.search.length > 0){
-                $scope.skills.push("Electricista");
-                $scope.skills.push("Plomero");
-                $scope.skills.push("Cantante");
+            if($scope.data.search && $scope.data.search.length >= 3){
+                $scope.filteredSkills = $scope.skills.filter($scope.filterSkills);
+            }else{
+                $scope.filteredSkills.length = 0;
             }
         }
         $scope.searchExperts = function(){
-            $scope.showExperts = true;
+
+            $scope.loading =  $ionicLoading.show({
+                template: Utility.getLoadingTemplate('Buscando Expertos')
+            });
+
+            setTimeout(function(){
+                $ionicLoading.hide();
+                $scope.showExperts = true;
+
+            }, 1500);
         }
 
         $scope.getLocalizedText = function(text){
