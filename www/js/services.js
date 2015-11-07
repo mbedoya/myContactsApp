3b5fa47fe42b0696bb5794c09d3d2e3db6fc5d45
mbedoya <mauricio.bedoya@gmail.com>
2015-11-07 15:01:11 -0500

    using displayName instead of name

--- www/js/services.js
+++ www/js/services.js
@@ -67,7 +67,7 @@ angular.module('laboru.services', [])
                         if(contacts[i].displayName && contacts[i].displayName.trim().length > 0 &&
                             contacts[i].name.givenName && contacts[i].name.givenName.trim().length > 0 &&
                             contacts[i].phoneNumbers && contacts[i].phoneNumbers.length > 0 && contacts[i].phoneNumbers[0].value.trim().length > 0){
-                            contactsArray.push({Name : contacts[i].name.givenName, LastName : contacts[i].name.familiyName, Mobile: contacts[i].phoneNumbers[0].value });
+                            contactsArray.push({Name : contacts[i].displayName, LastName : contacts[i].name.familiyName, Mobile: contacts[i].phoneNumbers[0].value });
                         }
                     }
 
