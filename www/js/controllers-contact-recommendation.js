bd5567caf020648e6b961c7e57bfa54056b799fc
mbedoya <mauricio.bedoya@gmail.com>
2016-03-13 11:07:34 -0500

    add skill improved

--- www/js/controllers-contact-recommendation.js
+++ www/js/controllers-contact-recommendation.js
@@ -7,6 +7,51 @@ controllersModule.controller('ContactRecommendationCtrl', function($scope, $root
         });
     };
 
+    $scope.filterSkills = function(value, index, ar){
+        return value.Name.toLowerCase().indexOf($scope.filterText) >= 0;
+    }
+
+    $scope.showChildren = function(skill){
+        if($scope.showThis(skill)){
+            $scope.hideChildren(skill);
+        }else{
+            $scope.displayedSkills.push(skill);
+        }
+    }
+
+    $scope.hideChildren = function(skill){
+        var skillIndex = $scope.displayedSkills.indexOf(skill);
+        $scope.displayedSkills.splice(skillIndex);
+    }
+
+    $scope.showThis = function(skill){
+
+        var skillFound = false;
+        for(var i=0; i<$scope.displayedSkills.length; i++){
+            if($scope.displayedSkills[i] == skill){
+                skillFound = true;
+                break;
+            }
+        }
+
+        return skillFound;
+    }
+
+    $scope.getChildSkills = function(parentSkill){
+        $scope.filterText = parentSkill.toLowerCase();
+        return $rootScope.skills.filter($scope.filterSkills);
+    }
+
+    $scope.initialize = function(){
+        $scope.displayedSkills = new Array();
+        $scope.model = { description: '' }
+        $scope.parentSkills = Utility.getParentCategories();
+    }
+
+    $scope.checkSkill = function(skill){
+        return $scope.skillRecommendedIndex(skill.ID) > -1;
+    }
+
     function findSkill(element, index, array, arg) {
         console.log(arg);
         return element.ID == arg;
@@ -65,11 +110,6 @@ controllersModule.controller('ContactRecommendationCtrl', function($scope, $root
         });
 
     });
-
-    $scope.initialize = function(){
-
-
-    }
 
     $scope.initialize();
 
